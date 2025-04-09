5ba5e7ff66bfa460b53fec93c3664752493ae17f
tomgxz <tomgxz.dev@gmail.com>
2025-04-09 14:47:02 +0100

    Add position values and launch config

--- oschandler.py
+++ oschandler.py
@@ -3,36 +3,58 @@ import logging
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
+'''
+pos5	 6.0345025	 2.9630268      x range -9.6 to 10.4    diff 20     per square 1.6r     half 0.83r      half 0.45
+pos5	 6.0344      2.9630866      z range -4.1 to 5.7     diff 9.8    per square 0.816r   half 0.4083r    half 0.21
+
+pos4	 0.69855934	 1.5890018      x range -10 to 11       diff 21     per square 1.75     half 0.875      half 0.45
+pos4	 0.69857436	 1.5889034      z range -3.3 to 6.5     diff 9.8    per square 0.816r   half 0.4083r    half 0.21
+
+pos3	-4.5688963	 2.3525424      x range -10.6 to 11     diff 21.6   per square 1.8      half 0.9        half 0.45
+pos3	-4.568933	 2.3526065      z range -3.85 to 5.6    diff 9.45   per square 0.7875   half 0.39375    half 0.21
+
+pos2	 8.82264    -2.9917376      x range -9.7 to 10.9    diff 20.6   per square 1.716r   half 0.8583r    half 0.45
+pos2	 8.822644	-2.9916458      z range -4.4 to 5.4     diff 9.8    per square 0.816r   half 0.4083r    half 0.21
+'''
+
+
+
 # first position must be smaller than second position
 CONFIG:dict = {
-    "pos2": {
-        "x1": 0.5845,
-        "z1": 0.0748,
-        "x2": 0.5847,
-        "z2": 0.0750,
-        "valid": False,
-    },
-    "pos3": {
-        "x1": 0,
-        "z1": 0,
-        "x2": 0,
-        "z2": 0,
-        "valid": False,
-    },
-    "pos4": {
-        "x1": 0,
-        "z1": 0,
-        "x2": 0,
-        "z2": 0,
-        "valid": False,
-    },
-    "pos5": {
-        "x1": 4.5897,
-        "z1": 2.8988,
-        "x2": 4.5899,
-        "z2": 2.8990,
-        "valid": False,
-    }
+    "positions": [
+        {
+            "x1": 5.58,
+            "x2": 6.48,
+            "z1": 3.17,
+            "z2": 3.75,
+            "valid": False,
+            "pointer": None,
+        },
+        {
+            "x1": 0.25,
+            "x2": 1.15,
+            "z1": 1.39,
+            "z2": 1.81,
+            "valid": False,
+            "pointer": None,
+        },
+        {
+            "x1": -4.95,
+            "x2": -4.11,
+            "z1":  2.09,
+            "z2":  2.90,
+            "valid": False,
+            "pointer": None,
+        },
+        {
+            "x1":  8.37,
+            "x2":  9.27,
+            "z1": -3.00,
+            "z2": -2.50,
+            "valid": False,
+            "pointer": None,
+        }
+    ] 
 }
 
 all_valid = False
@@ -48,31 +70,41 @@ def on_recieve(*args):
         return
 
     command, dispatcher_args, x, y, z = args
-    pos = dispatcher_args[0]
-    cfg = CONFIG[f"pos{pos}"]
+    pointer = dispatcher_args[0]
 
+    #logging.debug(f"POS - Recieved position for {pointer}: {x}, {z}")
 
-    if cfg['x1'] <= x <= cfg['x2'] and cfg['z1'] <= z <= cfg['z2']:
-        if cfg['valid']:
-            return
-        
-        cfg['valid'] = True
-        send_message(f'/escaperoom/challenge/3/pos/{pos}/valid')
+    invalid_position_exists = False
 
-        if CONFIG["pos2"]["valid"] and CONFIG["pos3"]["valid"] and CONFIG["pos4"]["valid"] and CONFIG["pos5"]["valid"]:
-            all_valid = True
+    for position in CONFIG["positions"]:
+        if position['x1'] <= x <= position['x2'] and position['z1'] <= z <= position['z2']:
+            if position['valid']:
+                continue
 
-            logging.debug('All positions are valid, sending success OSC...')
-            send_message(f'/escaperoom/challenge/3/success')
+            logging.debug(f"POS - Position valid for pointer {pointer}: {x}, {z}")
 
-    else:
-        if not cfg['valid']:
-            return
-        
-        cfg['valid'] = False
-        send_message(f'/escaperoom/challenge/3/pos/{pos}/invalid')
+            position['valid'] = True
+            position['pointer'] = pointer
 
-    logging.debug(f"Recieved position for {pos}: {x}, {z} - valid = {cfg}")
+            send_message(f'/escaperoom/challenge/3/pos/{pointer}/valid', 1)
+
+        else:
+            if position['valid'] and position['pointer'] == pointer:
+
+                logging.debug(f"POS - Position now invalid for pointer {pointer}: {x}, {z}")
+
+                position['valid'] = False
+                position['pointer'] = None
+                send_message(f'/escaperoom/challenge/3/pos/{pointer}/invalid', 1)
+
+    for position in CONFIG["positions"]:
+        if not position['valid']:
+            invalid_position_exists = True
+
+    if not invalid_position_exists:
+        all_valid = True
+        logging.debug('POS - All positions are valid, sending success OSC...')
+        send_message(f'/escaperoom/challenge/3/success', 1)
 
 
 if __name__ == "__main__":
